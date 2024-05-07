6070d27e4bef6cce4a739e9bd73cf798ce70b168
siva krishna <sivakrishna053936@gmail.com>
2024-05-07 22:54:31 +0530

    test;

---
+++ krishna.java
@@ -0,0 +1,16 @@
+public class krishna{
+
+        public static void main(String[] args)
+
+        { 
+
+    System.out.println( "short stories for children, fairytales,nursery rhymes and the fables The Threee little pigs,snowwhitetomthumb");
+    System.out.println(" teh main reasom for the brilliance of diamond -; is equal to the side of triangle so that the people cannot be the one that whoever the possibilities ");
+       
+          }
+      
+
+
+
+
+}
---
+++ shiny.java
@@ -0,0 +1,22 @@
+public class shiny{
+
+          public static void main(String[] args)
+          {
+            System.out.println("hi how are you siva");
+
+            System.out.println("yes iam fine what about you man");
+              
+            System.out.println("I too fine then where you now");
+           }
+
+}
+
+class Surya{
+
+                  public static void main(String[] args)
+                  
+                   
+              {    System.out.println("yes i am here only");
+}
+
+ }
