840eec12f3b7cade5c9ad36c15cec18f8228391c
siva krishna <sivakrishna053936@gmail.com>
2024-05-09 11:20:05 +0530

    test

---
+++ data types.java
@@ -0,0 +1,26 @@
+ class Siva{ 
+            public static void main(string[] args){
+
+
+            int a=100;
+            short b=700;
+            long  l=6301971794l;
+            float f=45000.367;
+            double d=45000.12365;
+            int[] i=80,20,30;
+            string s=("ram, krishna, siva");
+            
+
+
+            System.out.println(a);
+System.out.println(b);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(i);
+System.out.println(s);
+
+
+
+}
+}
